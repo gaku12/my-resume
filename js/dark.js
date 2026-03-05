4055cf608ebe7f66224a25fd6759a2e24df82e03
Gaku_12 <afrm2910@gmail.com>
2026-03-05 09:45:11 -0600

    Update_New_UI

--- js/dark.js
+++
@@ -1,8 +0,0 @@
-//dark mode
-const btnDark = document.getElementById('dark');
-
-btnDark.addEventListener('click' , () => {
-    document.body.classList.toggle('dark');
-    //change position of btn to switch style
-    btnDark.classList.toggle('active');
-});
